049984b3cc2d2bdcc6c66a74afe21223b34b37fe
Kharmun <148704982+Kharmun@users.noreply.github.com>
2023-11-15 15:44:17 +0500

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,18 @@
+import random
+guesses_made = 0
+name = input('Привет! Как тебя зовут?\n')
+number = random.randint(1,100)
+print ('Отлично, {0}, я загадал число в диапазоне от 1 до 100. Сможешь угадать?' .format(name))
+while guesses_made < random.randint(4,10):
+  guess = int(input('Ввели число: '))
+  guesses_made += 1
+  if guess < number:
+    print ('Твоё число меньше того, что я загадал.')
+  if guess > number:
+    print ('Твоё число больше загаданного мной.')
+  if guess == number:
+    break
+if guess == number:
+  print ('Ух ты, {0}! Ты угадал мое число, использовав {1} попыток!' .format(name, guesses_made))
+else:
+  print ('А вот и не угадал! Я загадал число {0}' .format(number))
